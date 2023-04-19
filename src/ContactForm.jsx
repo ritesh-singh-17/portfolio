d0688f1f9bfcca718f3e23b9c13c92f6d7a59430
riteshsingh633 <singh.riteshkumar0633@gmail.com>
2023-04-20 00:05:12 +0530

    Made the contact form working.

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -14,23 +14,23 @@ const ContactForm = () => {
 
     return (
         <>
-            <form>
+            <form action='https://formspree.io/f/xnqyljdg' method='POST' >
                 <div className="form-group">
                     <div className="formInput inputName">
                         <i className={name ? "icon" : "icon2"}><BsPersonFill /></i>
-                        <input className="input-field" type="text" placeholder="Name" onFocus={() => { return (setName(!name)) }} onBlur={() => { return (setName(!name)) }} />
+                        <input className="input-field" type="text" required placeholder="Name" name='Name' onFocus={() => { return (setName(!name)) }} onBlur={() => { return (setName(!name)) }} />
                     </div>
                     <div className="formInput inputEmail">
                         <i className={email ? "icon" : "icon2"}><MdEmail /></i>
-                        <input className="input-field" type="email" placeholder="Email" onFocus={() => { return (setEmail(!email)) }} onBlur={() => { return (setEmail(!email)) }} />
+                        <input className="input-field" type="email" required placeholder="Email" name='Email' onFocus={() => { return (setEmail(!email)) }} onBlur={() => { return (setEmail(!email)) }} />
                     </div>
                     <div className="formInput inputPhone">
                         <i className={phone ? "icon" : "icon2"}><FaPhoneAlt /></i>
-                        <input className="input-field" type="phone" placeholder="Phone" onFocus={() => { return (setPhone(!phone)) }} onBlur={() => { return (setPhone(!phone)) }} />
+                        <input className="input-field" type="phone" required placeholder="Phone" name='Phone' onFocus={() => { return (setPhone(!phone)) }} onBlur={() => { return (setPhone(!phone)) }} />
                     </div>
                     <div className="formInput inputMessage">
                         <i className={message ? "icon" : "icon2"}><RiMessageFill /></i>
-                        <textarea className="input-field" rows="04" cols="50" name="message" placeholder='Message..' onFocus={() => { return (setMessage(!message)) }} onBlur={() => { return (setMessage(!message)) }}></textarea>
+                        <textarea className="input-field" rows="04" cols="50" name="message" required placeholder='Message..' onFocus={() => { return (setMessage(!message)) }} onBlur={() => { return (setMessage(!message)) }}></textarea>
                     </div>
                     <div className="formInput inputSubmit">
                         <div className="inputSubmit1">
